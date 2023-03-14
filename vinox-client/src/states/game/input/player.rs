70620f009db92f0e7318b018ff85aaf8144a9234
Mylah Dee <vixeliz@icloud.com>
2023-03-13 23:08:07 -0500

    Janky normal

--- vinox-client/src/states/game/input/player.rs
+++ vinox-client/src/states/game/input/player.rs
@@ -263,7 +263,7 @@ pub fn interact(
                 &current_chunks,
                 &block_table,
             );
-            if let Some((chunk_pos, voxel_pos, _normal)) = hit {
+            if let Some((chunk_pos, voxel_pos, normal)) = hit {
                 let point = voxel_to_world(voxel_pos, chunk_pos);
 
                 if let Some(chunk_entity) = current_chunks.get_entity(chunk_pos) {
@@ -285,15 +285,25 @@ pub fn interact(
                                     || (point.y <= player_transform.translation.y - 1.0
                                         || point.y >= player_transform.translation.y + 1.0)
                                 {
+                                    // TODO: Get neighboring chunk when needed instead of just disregarding
+                                    let final_pos = UVec3::new(
+                                        (voxel_pos.x as f32 + normal.x) as u32,
+                                        (voxel_pos.y as f32 + normal.y) as u32,
+                                        (voxel_pos.z as f32 + normal.z) as u32,
+                                    )
+                                    .clamp(
+                                        UVec3::new(0, 0, 0),
+                                        UVec3::new(CHUNK_SIZE_ARR, CHUNK_SIZE_ARR, CHUNK_SIZE_ARR),
+                                    );
                                     chunk.chunk_data.add_block_state(&item_string);
                                     chunk.chunk_data.set_block(voxel_pos, &item_string);
                                     client.connection_mut().try_send_message(
                                         ClientMessage::SentBlock {
                                             chunk_pos,
                                             voxel_pos: [
-                                                voxel_pos.x as u8, // TODO: use normal and make sure to get neighbor chunk if needed
-                                                voxel_pos.y as u8,
-                                                voxel_pos.z as u8,
+                                                final_pos.x as u8, // TODO: use normal and make sure to get neighbor chunk if needed
+                                                final_pos.y as u8,
+                                                final_pos.z as u8,
                                             ],
                                             block_type: item_string,
                                         },
