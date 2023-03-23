5f6a88b8fb8dc4043244b85e6086f2e39c270371
Mylah Dee <vixeliz@icloud.com>
2023-03-23 17:40:12 -0500

    faster noise

--- vinox-server/src/game/world/generation.rs
+++ vinox-server/src/game/world/generation.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
-
 use bevy::prelude::*;
+use bracket_noise::prelude::*;
 use noise::{BasicMulti, Blend, MultiFractal, NoiseFn, OpenSimplex, RidgedMulti, RotatePoint};
+use std::collections::HashMap;
 // use rand::{rngs::StdRng, Rng, SeedableRng};
 use serde::{Deserialize, Serialize};
 use vinox_common::{
@@ -16,13 +16,14 @@ pub struct ToBePlaced(HashMap<IVec3, Vec<(UVec3, BlockDescriptor)>>);
 #[allow(clippy::type_complexity)]
 pub fn add_grass(
     raw_chunk: &mut ChunkData,
-    noisefn: &noise::Blend<
-        f64,
-        noise::RotatePoint<noise::RidgedMulti<noise::OpenSimplex>>,
-        noise::RotatePoint<noise::RidgedMulti<noise::OpenSimplex>>,
-        noise::BasicMulti<noise::OpenSimplex>,
-        3,
-    >,
+    // noisefn: &noise::Blend<
+    //     f64,
+    //     noise::RotatePoint<noise::RidgedMulti<noise::OpenSimplex>>,
+    //     noise::RotatePoint<noise::RidgedMulti<noise::OpenSimplex>>,
+    //     noise::BasicMulti<noise::OpenSimplex>,
+    //     3,
+    // >,
+    noisefn: &FastNoise,
     pos: IVec3,
 ) {
     for z in 0..=CHUNK_SIZE - 1 {
@@ -33,8 +34,8 @@ pub fn add_grass(
                     let full_z = z as i32 + ((CHUNK_SIZE as i32) * pos.z);
                     let full_y = y as i32 + ((CHUNK_SIZE as i32) * pos.y) + 1;
                     let noise_val =
-                        noisefn.get([full_x as f64, full_y as f64, full_z as f64]) * 45.152;
-                    if full_y as f64 > noise_val && raw_chunk.get_identifier(x, y, z) != "vinox:air"
+                        noisefn.get_noise3d(full_x as f32, full_y as f32, full_z as f32) * 120.412;
+                    if full_y as f32 > noise_val && raw_chunk.get_identifier(x, y, z) != "vinox:air"
                     {
                         let grass = BlockData::new("vinox".to_string(), "grass".to_string());
                         raw_chunk.set(x, y, z, grass);
@@ -96,29 +97,37 @@ pub fn add_grass(
 
 pub fn generate_chunk(pos: IVec3, seed: u32) -> RawChunk {
     //TODO: Switch to using ron files to determine biomes and what blocks they should use. For now hardcoding a simplex noise
-    let ridged_noise: RidgedMulti<OpenSimplex> =
-        RidgedMulti::new(seed).set_octaves(8).set_frequency(0.00622);
-    let d_noise: RidgedMulti<OpenSimplex> =
-        RidgedMulti::new(seed).set_octaves(6).set_frequency(0.00781);
-    let final_noise = Blend::new(
-        RotatePoint {
-            source: ridged_noise,
-            x_angle: 0.212,
-            y_angle: 0.321,
-            z_angle: -0.1204,
-            u_angle: 0.11,
-        },
-        RotatePoint {
-            source: d_noise,
-            x_angle: -0.124,
-            y_angle: -0.564,
-            z_angle: 0.231,
-            u_angle: -0.1151,
-        },
-        BasicMulti::<OpenSimplex>::new(seed)
-            .set_octaves(1)
-            .set_frequency(0.003415),
-    );
+    // let ridged_noise: RidgedMulti<OpenSimplex> =
+    //     RidgedMulti::new(seed).set_octaves(4).set_frequency(0.00622);
+    // let d_noise: RidgedMulti<OpenSimplex> = RidgedMulti::new(seed.wrapping_add(1))
+    //     .set_octaves(2)
+    //     .set_frequency(0.00781);
+    // let final_noise = Blend::new(
+    //     RotatePoint {
+    //         source: ridged_noise,
+    //         x_angle: 0.212,
+    //         y_angle: 0.321,
+    //         z_angle: -0.1204,
+    //         u_angle: 0.11,
+    //     },
+    //     RotatePoint {
+    //         source: d_noise,
+    //         x_angle: -0.124,
+    //         y_angle: -0.564,
+    //         z_angle: 0.231,
+    //         u_angle: -0.1151,
+    //     },
+    //     BasicMulti::<OpenSimplex>::new(seed)
+    //         .set_octaves(1)
+    //         .set_frequency(0.003415),
+    // );
+    let mut noise = FastNoise::seeded(seed as u64);
+    noise.set_noise_type(NoiseType::SimplexFractal);
+    noise.set_fractal_type(FractalType::RigidMulti);
+    noise.set_fractal_octaves(8);
+    noise.set_fractal_gain(0.55);
+    noise.set_fractal_lacunarity(2.0);
+    noise.set_frequency(0.0005);
 
     let mut raw_chunk = ChunkData::default();
     for x in 0..=CHUNK_SIZE - 1 {
@@ -127,9 +136,11 @@ pub fn generate_chunk(pos: IVec3, seed: u32) -> RawChunk {
                 let full_x = x as i32 + ((CHUNK_SIZE as i32) * pos.x);
                 let full_z = z as i32 + ((CHUNK_SIZE as i32) * pos.z);
                 let full_y = y as i32 + ((CHUNK_SIZE as i32) * pos.y);
+                // let noise_val =
+                //     final_noise.get([full_x as f64, full_y as f64, full_z as f64]) * 45.152;
                 let noise_val =
-                    final_noise.get([full_x as f64, full_y as f64, full_z as f64]) * 45.152;
-                if full_y as f64 <= noise_val {
+                    noise.get_noise3d(full_x as f32, full_y as f32, full_z as f32) * 120.412;
+                if full_y as f32 <= noise_val {
                     raw_chunk.set(
                         x,
                         y,
@@ -147,6 +158,6 @@ pub fn generate_chunk(pos: IVec3, seed: u32) -> RawChunk {
             }
         }
     }
-    add_grass(&mut raw_chunk, &final_noise, pos);
+    add_grass(&mut raw_chunk, &noise, pos);
     raw_chunk.to_raw()
 }
